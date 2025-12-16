95a2399292c0f756b3a7b73ce343ebdceae6cebe
Abdulsalam Jweda <a.jweda@tfgroup.ly>
2025-12-16 12:08:15 +0200

    fixed mailer reminder

--- routes/console.php
+++ routes/console.php
@@ -7,4 +7,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('documents:remind-expiry')->dailyAt('10:00');
+Schedule::command('documents:remind-expiry')->everyMinute();
